ead67f71e657245ef63f8665de8246ae50f34be2
Arzang Kasiri <arzangkasiri@gmail.com>
2018-12-22 10:05:32 -0800

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/greeting', (req, res) => {
-  res.send("hello");
+  res.send('<h1>hello world</h1>');
 });
 
 const PORT = process.env.PORT || 5000;
