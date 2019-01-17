6f1c81a3e2760e63de1ee782a93fa37877d27bfb
Arzang Kasiri <arzangkasiri@gmail.com>
2019-01-16 23:20:39 -0800

    built skeleton react client interface: header, landing, dashboard

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h3>
+        Your very own feedback friend!
+      </h3>
+      Collect feedback form from your users.
+    </div>
+  );
+};
+
+export default Landing;
